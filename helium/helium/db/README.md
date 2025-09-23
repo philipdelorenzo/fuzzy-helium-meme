12d03da31933837077f79df91435393d6139eb44
Philip De Lorenzo <philip.delorenzo@gmail.com>
2025-09-22 17:30:22 -0700

    chore: add corrected docs, code to reference PostgreSQL

--- helium/helium/db/README.md
+++ helium/helium/db/README.md
@@ -1,12 +1,12 @@
 # Database Connectivity
 
-In order to connect to the MongoDB database, there are some key factors in Doppler that restrict the connections of undesired environments, i.e. ~> `prd`.
+In order to connect to the PostgreSQL database, there are some key factors in Doppler that restrict the connections of undesired environments, i.e. ~> `prd`.
 
 ## Doppler Configs
 
-SERVICE_NAME --> ex: `utcore-hoa`
+SERVICE_NAME --> ex: `helium`
 
-DOPPLER_PROJECT --> unitytree-core<br/>
+DOPPLER_PROJECT --> helium-api-client<br/>
 DOPPLER_ENVIRONMENT --> `dev`|`stg`|`prd`<br/>
 DOPPLER_CONFIG --> `${DOPPLER_ENVIRONMENT}_${SERVICE_NAME}`
 
@@ -14,9 +14,6 @@ DOPPLER_CONFIG --> `${DOPPLER_ENVIRONMENT}_${SERVICE_NAME}`
 
 DP = Doppler Project<br/>
 env = Doppler Environment; i.e. ~> `dev`|`stg`|`prd`<br/>
-MONGODB_URI = `mongodb+srv://${unitytree-core-database.<DOPPLER_ENVIRONMENT>.MONGODB_USER}:${unitytree-core-database.<DOPPLER_ENVIRONMENT>.MONGODB_PASS}@${unitytree-core-database.dev.MONGODB_NAME}.${unitytree-core-database.<DOPPLER_ENVIRONMENT>.DATABASE_ID}.mongodb.net/?retryWrites=true&w=majority`
-
-_MONGODB_URI = `f"{MONGODB_URI}&appname={APP_NAME}"`
 
 ```mermaid
 
@@ -25,24 +22,22 @@ title: Doppler Database Configuration Flow
 ---
 
 flowchart LR;
-    subgraph unitytree-core
-        UTC[Config Base; i.e. ~> dev] ==> AppConfig("Config - Example: <br/>dev_utcore-hoa")
-        UTC
-        utcConfig["Config:<br/>MONGODB_URI
-        --
-        See MONGODB_URI above"]
-    end
-    subgraph unitytree-core-database
-        UTC-DB[Config Base; i.e. ~> dev] ==>|env|VARS[
-        DATABASE_ID
-        MONGO_DB_NAME
-        MONGODB_USER
-        MONGODB_PASS
-        ] ==> utcConfig
+    subgraph Helium
+        UTC[Config Base; i.e. ~> dev] ==> AppConfig("Config - Example: <br/>dev_")
     end
 
-    subgraph "Python FastAPI<br/>Example: utcore-hoa"
-        utcConfig ==> _MONGODB_URI["See Above:<br/>_MONGODB_URI"]
+    subgraph Helium AWS Aurora DB
+        UTC-DB[Config Base; i.e. ~> dev_] ==>|env|VARS[
+        POSTGRES_DB
+        POSTGRES_HOST
+        POSTGRES_PORT
+        POSTGRES_USER
+        POSTGRES_PASSWORD
+        ] ==> AuroraConfig
+    end
+
+    subgraph "Python FastAPI<br/>Example: Helium"
+        AuroraConfig
     end
 
     AppConfig ==> UTC-DB
--- helium/helium/db/__init__.py
+++ helium/helium/db/__init__.py
@@ -1,23 +1,62 @@
 import os
+import psycopg2
 
-import motor.motor_asyncio
+from typing import Any
 
-# MongoDB connection
-if os.environ.get("MONGODB_NAME"):
-    MONGODB_NAME = os.getenv("MONGODB_NAME")
+# PostgreSQL connection
+# These variables are coming from Doppler secrets manager
+# If the Doppler token is not set, or available, or correct, the application will not starts
+if os.environ.get("POSTGRES_HOST"):
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 else:
-    print("ERROR - MONGODB_NAME not set")
+    print("ERROR - POSTGRES_HOST not set")
     exit(1)
 
-if os.environ.get("MONGODB_URI"):
-    MONGODB_URI = os.getenv("MONGODB_URI")
+if os.environ.get("POSTGRES_PORT"):
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 else:
-    print("ERROR - MONGODB_URI not set")
+    print("ERROR - POSTGRES_PORT not set")
     exit(1)
 
-# Let's add the APP_NAME to the MONGDB_URI
-_MONGODB_URI = f"{MONGODB_URI}&appName={os.getenv('APP_NAME')}"
-client = motor.motor_asyncio.AsyncIOMotorClient(_MONGODB_URI)
+if os.environ.get("POSTGRES_DB"):
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+else:
+    print("ERROR - POSTGRES_DB not set")
+    exit(1)
 
-# Reference to the database
-database = client[MONGODB_NAME]  # type: ignore -- Replace 'my_database' with your DB name/DOPPLER
+if os.environ.get("POSTGRES_USER"):
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+else:
+    print("ERROR - POSTGRES_USER not set")
+    exit(1)
+
+if os.environ.get("POSTGRES_PASSWORD"):
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+else:
+    print("ERROR - POSTGRES_PASSWORD not set")
+    exit(1)
+
+try:
+    # Establishing the connection
+    # These variables are coming from Doppler secrets manager
+    conn = psycopg2.connect(
+        host=POSTGRES_HOST,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        database=POSTGRES_DB,
+        port=POSTGRES_PORT
+    )
+    
+    # Create a cursor object to execute queries
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT version()")
+        db_version: Any = cursor.fetchone()
+        print(f"Connected to Aurora PostgreSQL. Database version: {db_version[0]}")
+
+except psycopg2.Error as e:
+    print(f"Failed to connect to the database: {e}")
+
+finally:
+    if 'conn' in locals() and not conn.closed:
+        conn.close()
+        print("Database connection closed.")
